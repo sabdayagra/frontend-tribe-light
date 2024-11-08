59f08bf422337243db388616c6a9f13441412de9
sabdayagra <msabdayagra@gmail.com>
2024-11-08 19:45:41 +0700

    finished

---
+++ src/sections/Footer.tsx
@@ -0,0 +1,36 @@
+import logo from "@/assets/logosaas.png";
+import SocialX from "@/assets/social-x.svg";
+import SocialInsta from "@/assets/social-insta.svg";
+import SocialLinkedIn from "@/assets/social-linkedin.svg";
+import SocialPin from "@/assets/social-pin.svg";
+import SocialYoutube from "@/assets/social-youtube.svg";
+
+import Image from "next/image";
+
+export const Footer = () => {
+  return (
+    <footer className="bg-black text-[#BCBCBC] text-sm py-10 text-center">
+      <div className="container">
+        <div className="inline-flex relative before:content-[''] before:top-2 before:bottom-0 before:h-full before:blur before:w-full before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] before:bg-red-500 before:absolute">
+          <Image src={logo} alt="Logo" height={40} className="relative" />
+        </div>
+        <nav className="flex flex-col gap-6 mt-6 md:flex-row md:justify-center">
+          <a href="#">About</a>
+          <a href="#">Features</a>
+          <a href="#">Costumers</a>
+          <a href="#">Pricing</a>
+          <a href="#">Help</a>
+          <a href="#">Careers</a>
+        </nav>
+        <div className="flex justify-center gap-6 mt-6">
+          <SocialX />
+          <SocialInsta />
+          <SocialLinkedIn />
+          <SocialPin />
+          <SocialYoutube />
+        </div>
+        <p className="mt-6">&copy; 2024 Your Company, Inc. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
